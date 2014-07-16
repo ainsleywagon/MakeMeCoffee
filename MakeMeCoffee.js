c73e23618eb64b89ff524e458b20160eb78f91d0
Imanol Aranzadi <iiaranzadi@gmail.com>
2014-07-16 08:05:25 -0700

    js fixed and some css bootstrap code added

--- MakeMeCoffee.js
+++ MakeMeCoffee.js
@@ -1,31 +1,21 @@
-<<<<<<< HEAD
-// $(document).ready(function() {
-//
-//     var makeMeCoffee = function(ounces) {
-//       var beanQty = Math.round(ounces * 1.2083333);
-//       var waterQty = Math.round(beanQty * 16.4285714);
-//       var firstPour = beanQty * 2;
-//       var secondPour = waterQty - firstPour;
-//
-//       alert('tantos coffee beans'+ secondPour);
-//     };
-//
-//     $( "#forma" ).submit(function( event ) {
-//
-//       makeMeCoffee($("#ounces").val());
-//     });
-// });
-=======
-var ounces = 5;
+$(document).ready(function() {
 
-var makeMeCoffee = function(ounces) {
-  var beanQty = Math.round(ounces * 1.2083333).toFixed(2);
-  var waterQty = Math.round(beanQty * 16.4285714).toFixed(2);
-  var firstPour = (beanQty * 2).toFixed(2);
-  var secondPour = (waterQty - firstPour).toFixed(2);
+    $( "#forma" ).submit(function( event ) {
+      event.preventDefault();
+      makeMeCoffee($("#ounces").val());
+    });
 
-  console.log(beanQty + 'g', firstPour + 'g', secondPour + 'g');
-};
+    var makeMeCoffee = function(ounces) {
+      var beanQty = Math.round(ounces * 1.2083333).toFixed(2);
+      var waterQty = Math.round(beanQty * 16.4285714).toFixed(2);
+      var firstPour = (beanQty * 2).toFixed(2);
+      var secondPour = (waterQty - firstPour).toFixed(2);
 
-makeMeCoffee(ounces);
->>>>>>> c573baaa299f0c09726eadf99d53764d6f0d3139
+      var htmlString = "You need " + beanQty + 'g of coffee';
+      $('#directions').text(htmlString);
+
+      console.log('You need ' + beanQty + 'g of coffee');
+    };
+
+
+});
