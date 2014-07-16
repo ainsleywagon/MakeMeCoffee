0b1a18bd4ccfcb2c47650ffee85e63ca08c71525
Imanol Aranzadi <iiaranzadi@gmail.com>
2014-07-16 01:48:23 -0700

    erase js script from index.html and added to MakeMeCoffee.js

--- MakeMeCoffee.js
+++ MakeMeCoffee.js
@@ -1,12 +1,16 @@
-var ounces = 5;
-
-var makeMeCoffee = function(ounces) {
-  var beanQty = Math.round(ounces * 1.2083333);
-  var waterQty = Math.round(beanQty * 16.4285714);
-  var firstPour = beanQty * 2;
-  var secondPour = waterQty - firstPour;
-
-  console.log(beanQty, firstPour, secondPour);
-};
-
-makeMeCoffee(ounces);
+// $(document).ready(function() {
+//
+//     var makeMeCoffee = function(ounces) {
+//       var beanQty = Math.round(ounces * 1.2083333);
+//       var waterQty = Math.round(beanQty * 16.4285714);
+//       var firstPour = beanQty * 2;
+//       var secondPour = waterQty - firstPour;
+//
+//       alert('tantos coffee beans'+ secondPour);
+//     };
+//
+//     $( "#forma" ).submit(function( event ) {
+//
+//       makeMeCoffee($("#ounces").val());
+//     });
+// });
